94933c8164509db917ed8e363ee028480af6c1bc
xel <haralds.meyers@protonmail.com>
2024-10-15 18:05:14 +0100

    Removed old broken raycaster

---
+++ mood/Player.cs
@@ -0,0 +1,119 @@
+namespace mood;
+
+public class Player
+{
+    public int X = 5;
+    public int Y = 95;
+    public double Direction = 90;
+    // public int Health = 100;
+    // public string Name = "";
+    // public int Level = 1;
+    // public int Xp = 0;
+    // public int[,] Items = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } }; //item id, item count.
+
+    public double CastRay(bool[,] map)
+{
+    double xCalc, yCalc, distCalc, xStep, yStep, xInitial, yInitial, xDiff, yDiff;
+
+    // Vertical collision detection
+    xCalc = X + 0.5;
+    yCalc = Y + 0.5;
+    xStep = Math.Cos(Direction * (Math.PI / 180)) > 0 ? 1 : -1;
+    yStep = Math.Tan(Direction * (Math.PI / 180)) * xStep;
+
+    xInitial = xStep / 2;
+    yInitial = Math.Tan(Direction * (Math.PI / 180)) * xInitial;
+    xCalc += xInitial;
+    yCalc += yInitial;
+
+    if (xCalc > map.GetLength(0) - 1) xCalc = map.GetLength(0) - 1;
+    else if (xCalc < 0) xCalc = 0;
+
+    if (yCalc > map.GetLength(1) - 1) yCalc = map.GetLength(1) - 1;
+    else if (yCalc < 0) yCalc = 0;
+
+    if (!map[Convert.ToInt32(xCalc), Convert.ToInt32(yCalc)])
+    {
+        bool collided = false;
+        while (!collided)
+        {
+            xCalc += xStep;
+            yCalc += yStep;
+            if (yCalc <= 0 || yCalc >= map.GetLength(1) - 1 || xCalc < 0 || xCalc >= map.GetLength(0) - 1)
+            {
+                collided = true;
+            }
+            else if (map[Convert.ToInt32(xCalc), Convert.ToInt32(yCalc)])
+            {
+                collided = true;
+            }
+        }
+    }
+    xDiff = xCalc - X;
+    distCalc = xDiff / Math.Cos(Direction * (Math.PI / 180));
+    double vdistance = Math.Truncate(distCalc);
+
+    // Horizontal collision detection
+    xCalc = X + 0.5;
+    yCalc = Y + 0.5;
+    yStep = Math.Sin(Direction * (Math.PI / 180)) > 0 ? 1 : -1;
+    xStep = yStep / Math.Tan(Direction * (Math.PI / 180));
+
+    yInitial = yStep / 2;
+    xInitial = yInitial / Math.Tan(Direction * (Math.PI / 180));
+    xCalc += xInitial;
+    yCalc += yInitial;
+
+    if (xCalc > map.GetLength(0) - 1) xCalc = map.GetLength(0) - 1;
+    else if (xCalc < 0) xCalc = 0;
+
+    if (yCalc > map.GetLength(1) - 1) yCalc = map.GetLength(1) - 1;
+    else if (yCalc < 0) yCalc = 0;
+
+    if (!map[Convert.ToInt32(xCalc), Convert.ToInt32(yCalc)])
+    {
+        bool collided = false;
+        while (!collided)
+        {
+            xCalc += xStep;
+            yCalc += yStep;
+            if (yCalc <= 0 || yCalc >= map.GetLength(1) - 1 || xCalc < 0 || xCalc >= map.GetLength(0) - 1)
+            {
+                collided = true;
+            }
+            else if (map[Convert.ToInt32(xCalc), Convert.ToInt32(yCalc)])
+            {
+                collided = true;
+            }
+        }
+    }
+    yDiff = yCalc - Y;
+    distCalc = yDiff / Math.Sin(Direction * (Math.PI / 180));
+    double hdistance = Math.Truncate(distCalc);
+
+    double distance = Math.Min(Math.Abs(hdistance), Math.Abs(vdistance));
+    return distance;
+}
+    
+    private double Tan(double angle)
+    {
+        double radians = angle * (Math.PI/180);
+        double result = Math.Tan(radians);
+        return result;
+    }
+    
+    private double Sin(double angle)
+    {
+        double radians = angle * (Math.PI/180);
+        double result = Math.Sin(radians);
+        return result;
+    }
+    
+    private double Cos(double angle)
+    {
+        double radians = angle * (Math.PI/180);
+        double result = Math.Cos(radians);
+        return result;
+    }
+    
+}
---
+++ mood/Program.cs
@@ -0,0 +1,144 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace mood;
+
+public static class Program {
+    
+    private static Player player = new Player();
+    private static ConsoleColor background = ConsoleColor.Gray;
+    private const int fov = 90;
+    private const int wallHeight = 60;
+    //screen = 50x20
+
+    static void Main()
+    {
+        bool[,] map = DrawMap();
+        do
+        {
+            Print(map);
+            Move();
+
+        } while (true);
+    }
+
+    static void Move()
+    {
+        ConsoleKeyInfo key;
+        key = Console.ReadKey();
+        switch (key.Key)
+        {
+            case ConsoleKey.Escape:
+                return;
+                    
+            case ConsoleKey.RightArrow:
+                player.Direction += 10;
+                break;
+                
+            case ConsoleKey.LeftArrow:
+                player.Direction -= 5;
+                break;
+
+            case ConsoleKey.W:
+                if (player.X <= 95)
+                { 
+                    player.X += 5;
+                }
+                break;
+
+            case ConsoleKey.S:
+                if (player.X >= 5)
+                {
+                    player.X -= 5;
+                }
+                break;
+                
+            case ConsoleKey.A:
+                if (player.Y <= 95)
+                {
+                    player.Y += 5;
+                }
+                break;
+                
+            case ConsoleKey.D:
+                if (player.Y >= 5)
+                {
+                    player.Y -= 5;
+                }
+                break;
+        }
+    }
+    static bool[,] DrawMap()
+    {
+        
+        const int mapX = 100;
+        const int mapY = 100;
+        bool[,] map = new bool[mapX, mapY];
+
+        for (int x = 0; x < mapX; x++)
+        {
+            for (int y = 0; y < mapY; y++)
+            {
+                map[x, y] = false;
+            }
+        }
+        
+        map[25, 25] = true;
+        map[75, 75] = true;
+        
+        return map;
+    }
+
+    static void Print(bool[,] map)
+    {
+        Console.Clear();
+        const int screenX = 256;
+        const int screenY = 40;
+        bool[,] screen = new bool[screenX, screenY];
+        double oldDir = player.Direction;
+        
+        
+        player.Direction -= (fov / 2f);
+        for (int x = 0; x <= screenX - 1; x++)
+        {
+            double step = Convert.ToDouble(fov) / Convert.ToDouble(screenX);
+            double distance = player.CastRay(map);
+            double halfLine = distance * Math.Tan(0.5 * fov);
+            double lineRatio = ((0.5*wallHeight) / halfLine);
+
+            
+            for (int y = 0; y <= screenY - 1; y++)
+            {
+                bool valid = (y > (screenY / 2f) - (0.5 * (screenY * lineRatio))) && (y < (screenY / 2f) + (0.5 * (screenY * lineRatio)));
+                if (valid)
+                {
+                    screen[x, y] = true; 
+                }
+                else
+                {
+                    screen[x, y] = false;
+                }
+            }
+
+            player.Direction += step;
+        }
+        player.Direction = oldDir;
+
+
+        for (int y = 0; y < screenY ; y++)
+        {
+            for (int x = 0; x < screenX ; x++)
+            {
+                if (screen[x, y])
+                {
+                    Console.Write("o");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(player.Direction);
+    }
+}
